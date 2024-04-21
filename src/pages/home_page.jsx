12a99b0e32564dc46aa86fc7b38358124735caf3
irfanplazuardi <irfanplazuardi@gmail.com>
2024-04-21 08:34:27 +0800

    template folder structure

---
+++ src/pages/home_page.jsx
@@ -0,0 +1,10 @@
+const Home_page = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-orange-500">Hello world!</h1>
+      <h2>This is home page</h2>
+    </div>
+  );
+};
+
+export default Home_page;
