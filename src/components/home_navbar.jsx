72c00bd96ac413db0fa0eccc509aafea0037d414
irfanplazuardi <irfanplazuardi@gmail.com>
2024-04-24 21:18:34 +0800

    design homepage UI

---
+++ src/components/home_navbar.jsx
@@ -0,0 +1,25 @@
+const HomeNavbar = () => {
+  return (
+    <nav>
+      <div className="flex justify-between items-baseline w-11/12 m-auto mx-20 my-5">
+        <div className="flex items-center">
+          <img src="src/assets/svg_image/Vogue_school_logo.svg" alt="logo" />
+        </div>
+        <div className="flex items-center space-x-10">
+          <a href="#" className="hover:text-yellow-400 text-xl">
+            Course
+          </a>
+          <a href="#" className="hover:text-yellow-400 text-xl">
+            About
+          </a>
+          <a href="/login" className="hover:text-yellow-400 text-xl">
+            Login
+          </a>
+        </div>
+      </div>
+      <hr className="w-12/12 mt-2 border-black m-10" />
+    </nav>
+  );
+};
+
+export default HomeNavbar;
