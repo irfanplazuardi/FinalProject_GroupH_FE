6a838958a0c8cc4dadd46a7d36095902eeab00c6
irfanplazuardi <irfanplazuardi@gmail.com>
2024-04-25 22:00:26 +0800

    create dashboard folder structure

--- src/assets/VogueSchoolLogoLarge.jsx
+++ src/assets/VogueSchoolLogoLarge.jsx
@@ -3,16 +3,14 @@ import React from "react";
 const VogueSchoolLogoLarge = () => {
   return (
     <div className="text-center">
-      <div class="flex items-center justify-center h-10 text-white uppercase font-bold text-3xl tracking-widest">
-        <div class="mr-1">V</div>
-        <div class="mr-1">O</div>
-        <div class="mr-1">G</div>
-        <div class="mr-1">U</div>
-        <div class="mr-1">E</div>
+      <div className="flex items-center justify-center h-10 text-white uppercase font-bold text-3xl tracking-widest">
+        <div className="mr-1">V</div>
+        <div className="mr-1">O</div>
+        <div className="mr-1">G</div>
+        <div className="mr-1">U</div>
+        <div className="mr-1">E</div>
       </div>
-      <div className="text-xl text-white rounded-full">
-        School
-      </div>
+      <div className="text-xl text-white rounded-full">School</div>
     </div>
   );
 };
--- src/components/logo_large.jsx
+++ src/components/logo_large.jsx
@@ -3,19 +3,15 @@ import React from "react";
 const VogueSchoolLogoLarge = () => {
   return (
     <div className="text-center p-7">
-      <div class="flex items-center justify-center h-10 text-white uppercase font-bold text-7xl tracking-widest">
-        <div class="mr-1">V</div>
-        <div class="mr-1">O</div>
-        <div class="mr-1">G</div>
-        <div class="mr-1">U</div>
-        <div class="mr-1">E</div>
+      <div className="flex items-center justify-center h-10 text-white uppercase font-bold text-7xl tracking-widest">
+        <div className="mr-1">V</div>
+        <div className="mr-1">O</div>
+        <div className="mr-1">G</div>
+        <div className="mr-1">U</div>
+        <div className="mr-1">E</div>
       </div>
-      <div className="text-4xl font-medium text-white rounded-full mt-2 px-2 py-1">
-        School
-      </div>
-      <div className="text-xl font-light text-white rounded-full px-2">
-        Beyond the World!
-      </div>
+      <div className="text-4xl font-medium text-white rounded-full mt-2 px-2 py-1">School</div>
+      <div className="text-xl font-light text-white rounded-full px-2">Beyond the World!</div>
     </div>
   );
 };
