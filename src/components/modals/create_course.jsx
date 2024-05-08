e2be2c31d403863dadf2c5debf20796cf56c9d03
Egbert Felica Wibianto <137465044+Orang9@users.noreply.github.com>
2024-05-09 05:19:04 +0700

    Create/update course modal (#36)
    * make it responsive for laptops
    
    * make UI pop up edit course modul responsive for laptops
    
    * fixed style create course modal
    
    * fixed style edit course modal

--- src/components/modals/create_course.jsx
+++ src/components/modals/create_course.jsx
@@ -41,48 +41,48 @@ const AddCourseModal = () => {
       {isOpen && (
         <form onSubmit={handleSubmit}>
           <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-50">
-            <div className="grid grid-cols-4 grid-rows-6 gap-4 bg-white p-8 rounded-lg w-[85rem] h-[55rem]">
+            <div className="grid grid-cols-4 grid-rows-6 gap-4 bg-white p-8 rounded-lg w-[80vh] h-[90vh]">
               <div className="col-span-4 row-span-1">
-                <h2 className="text-5xl font-bold">Create Course</h2>
+                <h2 className="text-xl font-bold">Create Course</h2>
               </div>
 
               <div className="col-span-1 row-span-5 flex flex-col justify-between gap-4">
-                <div>
-                  <label className="text-xl">Course Name</label>
-                  <input type="text" name="courseName" value={courseInfo.courseName} onChange={handleChange} placeholder="Maths" className="w-[20rem] p-2 border border-gray-300 bg-gray-200 rounded-lg" required />
+                <div className="flex flex-col">
+                  <label className="text-ml">Course Name</label>
+                  <input type="text" name="courseName" value={courseInfo.courseName} onChange={handleChange} placeholder="Maths" className="w-[30vh] p-2 border border-gray-300 bg-gray-200 rounded-lg" required />
                 </div>
-                <div>
-                  <label className="text-xl">Subject</label>
-                  <input type="text" name="subject" value={courseInfo.subject} onChange={handleChange} placeholder="Aljabar" className="w-[20rem] p-2 border border-gray-300 bg-gray-200 rounded-lg " required />
+                <div className="flex flex-col">
+                  <label className="text-ml">Subject</label>
+                  <input type="text" name="subject" value={courseInfo.subject} onChange={handleChange} placeholder="Aljabar" className="w-[30vh] p-2 border border-gray-300 bg-gray-200 rounded-lg " required />
                 </div>
-                <div>
-                  <label className="text-xl">Grade</label>
-                  <input type="text" name="grade" value={courseInfo.grade} onChange={handleChange} placeholder="SMA " className="w-[20rem] p-2 border border-gray-300 bg-gray-200 rounded-lg " required />
+                <div className="flex flex-col">
+                  <label className="text-ml">Grade</label>
+                  <input type="text" name="grade" value={courseInfo.grade} onChange={handleChange} placeholder="SMA " className="w-[30vh] p-2 border border-gray-300 bg-gray-200 rounded-lg " required />
                 </div>
-                <div>
-                  <label className="text-xl">Upload Image</label>
-                  <input type="file" name="picture" value={courseInfo.picture} onChange={handleChange} placeholder="Picture" className="w-[20rem] h-[20rem] p-2 border border-gray-300 bg-gray-200 rounded-lg " required />
+                <div className="flex flex-col">
+                  <label className="text-ml">Upload Image</label>
+                  <input type="file" name="picture" value={courseInfo.picture} onChange={handleChange} placeholder="Picture" className="w-[30vh] h-[20vh] p-2 border border-gray-300 bg-gray-200 rounded-lg " required />
                 </div>
               </div>
 
-              <div className="col-span-3 row-span-5 pl-5 ">
-                <label className="text-xl">Description</label>
+              <div className="flex flex-col col-span-3 row-span-5 pl-[15vh] ">
+                <label className="text-ml">Description</label>
                 <textarea
                   name="description"
                   value={courseInfo.description}
                   onChange={handleChange}
                   cols="30"
                   rows="10"
-                  className="w-full h-full p-2 border border-gray-300 rounded-lg bg-gray-200 "
+                  className="w-[40vh] h-full p-2 border border-gray-300 rounded-lg bg-gray-200 "
                   placeholder="Describe the course here"
                   required></textarea>
               </div>
 
               <div className="col-span-4 row-span-1 flex justify-center items-center mt-10">
-                <button type="submit" className="bg-black hover:bg-blue-800 text-white font-medium py-4 px-[4rem] rounded-lg mr-4">
+                <button type="submit" className="bg-black hover:bg-blue-800 text-white font-medium py-4 px-[4vh] rounded-lg mr-4">
                   Create
                 </button>
-                <button className="bg-red-500 hover:bg-gray-400 text-black font-medium py-4 px-[4rem] rounded-lg" onClick={toggleModal}>
+                <button className="bg-red-500 hover:bg-gray-400 text-black font-medium py-4 px-[4vh] rounded-lg" onClick={toggleModal}>
                   Cancel
                 </button>
               </div>
