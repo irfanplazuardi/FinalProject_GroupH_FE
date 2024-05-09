2fdb3de25d0e7847cc917ed427f715d20f1f4755
Egbert Felica Wibianto <egbert.fewi@gmail.com>
2024-05-09 21:56:19 +0700

    refactor course list table

--- src/components/tables/course_table.jsx
+++ src/components/tables/course_table.jsx
@@ -1,31 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import apiService from "../../api/api_service";
 import Container from "../container";
 import AddCourseModal from "../modals/create_course";
 import EditCourseModal from "../modals/edit_course";
 import DeleteModal from "../modals/delete_course";
 
 const CourseTable = () => {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    apiService
+      .getCourses()
+      .then((data) => {
+        setCourses(data.courses);
+      })
+      .catch((error) => {
+        console.error("Error fetching courses:", error);
+      });
+  }, []);
   return (
     <>
       <Container className="flex justify-between ">
         <Container className="flex gap-5">
           <select className="flex justify-end py-3 px-4 rounded-2xl mb-5 bg-gray-300">
-            <option value="matematika"> Course</option>
+            <option value="course"> Course</option>
             <option value="fisika">fisika</option>
             <option value="kimia">kimia</option>
             <option value="biologi">biologi</option>
-          </select>
-          <select className="flex justify-end py-3 px-4 rounded-2xl mb-5 bg-gray-300">
-            <option value="matematika"> Semester</option>
-            <option value="fisika">1</option>
-            <option value="kimia">2</option>
-            <option value="biologi">3</option>
-          </select>
-          <select className="flex justify-end py-3 px-4 rounded-2xl mb-5 bg-gray-300">
-            <option value="matematika"> Student</option>
-            <option value="fisika">a</option>
-            <option value="kimia">b</option>
-            <option value="biologi">c</option>
           </select>
         </Container>
         <Container className="mx-2">
@@ -43,23 +44,20 @@ const CourseTable = () => {
                 <th className="border border-black px-4 py-2">Grade</th>
                 <th className="border border-black px-4 py-2">Picture</th>
                 <th className="border border-black px-4 py-2">Description</th>
-                <th className="border border-black px-4 py-2">Created At</th>
-                <th className="border border-black px-4 py-2">Updated At</th>
                 <th className="border border-black px-4 py-2">Actions</th>
               </tr>
             </thead>
             <tbody>
-              <tr className="bg-white">
-                <td className="border border-black px-4 py-2">1</td>
-                <td className="border border-black px-4 py-2">Calculus</td>
-                <td className="border border-black px-4 py-2">Matematika</td>
-                <td className="border border-black px-4 py-2">SMA</td>
+              {courses.map((course) => (
+                <tr className="bg-white">
+                <td className="border border-black px-4 py-2">{course.course_id}</td>
+                <td className="border border-black px-4 py-2">{course.course_name}</td>
+                <td className="border border-black px-4 py-2">{course.course_subjects}</td>
+                <td className="border border-black px-4 py-2">{course.course_grade}</td>
                 <td className="border border-black px-4 py-2">
                   <img src="https://gravatar.com/avatar/27205e5c51cb03f862138b22bcb5dc20f94a342e744ff6df1b8dc8af3c865109" alt="avatar" />
                 </td>
-                <td className="border border-black px-4 py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, accusamus!</td>
-                <td className="border border-black px-4 py-2">2022-01-01 00:00:00</td>
-                <td className="border border-black px-4 py-2">2022-01-01 00:00:00</td>
+                <td className="border border-black px-4 py-2">{course.course_description}</td>
                 <td className="border border-black px-4 py-2  ">
                   <div className="flex justify-center ">
                     <EditCourseModal />
@@ -67,6 +65,7 @@ const CourseTable = () => {
                   </div>
                 </td>
               </tr>
+              ))}
             </tbody>
           </table>
         </Container>
