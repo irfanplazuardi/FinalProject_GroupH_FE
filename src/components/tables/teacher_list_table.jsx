509b767c9076dcd40f66e542c269ed98fb0d59e7
Egbert Felica Wibianto <egbert.fewi@gmail.com>
2024-05-09 17:29:06 +0700

    refactor teacher list table

--- src/components/tables/teacher_list_table.jsx
+++ src/components/tables/teacher_list_table.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import apiService from "../../api/api_service";
 import EditTeacherModal from "../modals/edit_teacher";
 import DeleteModal from "../modals/delete_course";
 
 const TeacherListTable = () => {
+  const [teachers, setTeachers] = useState([]);
+
+  useEffect(() => {
+    apiService
+      .getTeachers()
+      .then((data) => {
+        setTeachers(data.teachers);
+      })
+      .catch((error) => {
+        console.error("Error fetching teachers:", error);
+      });
+  }, []);
+
   return (
     <>
       <table className="border border-black m-10">
@@ -13,36 +27,36 @@ const TeacherListTable = () => {
             <th className="border border-black px-4 py-2">PHONE</th>
             <th className="border border-black px-4 py-2">EMAIL</th>
             <th className="border border-black px-4 py-2">Picture</th>
-            <th className="border border-black px-4 py-2">Created At</th>
-            <th className="border border-black px-4 py-2">Updated At</th>
             <th className="border border-black px-4 py-2">Actions</th>
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-white">
-            <td className="border border-black px-4 py-2">1</td>
-            <td className="border border-black px-4 py-2">Rudi</td>
-            <td className="border border-black px-4 py-2">08543671</td>
-            <td className="border border-black px-4 py-2">rudi@gmail.com</td>
-            <td className="border border-black px-4 py-2">
-              <img
-                src="https://gravatar.com/avatar/27205e5c51cb03f862138b22bcb5dc20f94a342e744ff6df1b8dc8af3c865109"
-                alt="avatar"
-              />
-            </td>
-            <td className="border border-black px-4 py-2">
-              2022-01-01 00:00:00
-            </td>
-            <td className="border border-black px-4 py-2">
-              2022-01-01 00:00:00
-            </td>
-            <td className="border border-black px-4 py-2  ">
-              <div className="flex justify-center ">
-                <EditTeacherModal />
-                <DeleteModal />
-              </div>
-            </td>
-          </tr>
+          {teachers.map((teacher) => (
+            <tr className="bg-white">
+              <td className="border border-black px-4 py-2">
+                {teacher.teacher_id}
+              </td>
+              <td className="border border-black px-4 py-2">
+                {teacher.teacher_name}
+              </td>
+              <td className="border border-black px-4 py-2">{teacher.phone}</td>
+              <td className="border border-black px-4 py-2">
+                {teacher.teacher_email}
+              </td>
+              <td className="border border-black px-4 py-2">
+                <img
+                  src="https://gravatar.com/avatar/27205e5c51cb03f862138b22bcb5dc20f94a342e744ff6df1b8dc8af3c865109"
+                  alt="avatar"
+                />
+              </td>
+              <td className="border border-black px-4 py-2  ">
+                <div className="flex justify-center ">
+                  <EditTeacherModal />
+                  <DeleteModal />
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
