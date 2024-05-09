e6deac3cb44d6e1c8da69ce0f063f15e890ed747
Egbert Felica Wibianto <137465044+Orang9@users.noreply.github.com>
2024-05-09 20:37:36 +0700

    Student teacher course (#38)
    * make it responsive for laptops
    
    * make UI pop up edit course modul responsive for laptops
    
    * fixed style create course modal
    
    * fixed style edit course modal
    
    * Added async get Courses function to retrieve course data via API using authorization token
    
    * delete unnecessary code
    
    ---------
    
    Co-authored-by: Irfan Lazuardi <106393521+irfanplazuardi@users.noreply.github.com>

--- src/api/api_service.jsx
+++ src/api/api_service.jsx
@@ -3,8 +3,6 @@ import axios from "axios";
 const role = localStorage.getItem("role");
 const user_id = localStorage.getItem("user_id");
 const access_token = localStorage.getItem("access_token");
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxNTIxNjU2MiwianRpIjoiYWEwZjM0M2YtNDZkNC00NjYwLTk3MWMtNjI2MmFkMThiNzVkIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6NCwibmJmIjoxNzE1MjE2NTYyLCJjc3JmIjoiMTQ0MzRhYTItNTliZS00ZjkyLTk4NzQtODE3ZmI5NDkwZjMwIiwiZXhwIjoxNzE1MjE3NDYyLCJ1c2VybmFtZSI6ImlyZmFuIiwicm9sZSI6InN0dWRlbnQifQ.RXQWUTZJyP1GHHM6Bk07VCV0SVXLsYmb-pxgOSX5mqI"; // Create Axios instance with base URL and default headers
 const api = axios.create({
   baseURL: "https://adorable-serenity-production.up.railway.app",
   headers: {
@@ -29,6 +27,20 @@ const apiService = {
   async getAnnouncements() {
     try {
       const response = await api.get("/announcement", {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("API Error:", error);
+      throw error;
+    }
+  },
+
+  async getCourses() {
+    try {
+      const response = await api.get("/courses", {
         headers: {
           Authorization: `Bearer ${access_token}`,
         },
