ebcdf1def5310dbf31f4e89e150538dfaf06e851
Egbert Felica Wibianto <egbert.fewi@gmail.com>
2024-05-11 03:29:18 +0700

    add delete function for teacher account

--- src/api/api_service.jsx
+++ src/api/api_service.jsx
@@ -243,6 +243,21 @@ const apiService = {
       throw error;
     }
   },
+  
+  async deleteTeacherID(teacher_id, access_token) {
+    try {
+      const response = await api.delete(`/teachers/${teacher_id}`, {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      });
+      return response.data;
+
+    } catch (error) {
+      console.error("API Error:", error);
+      throw error;
+    }
+  },
 
   // Add more methods as needed (PUT, DELETE, etc.)
 };
